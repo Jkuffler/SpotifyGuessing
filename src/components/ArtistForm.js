4f3a8405c45f9f8315d22c4c7fe621dfbb0a50ad
Jason Kuffler <jakuffler@gmail.com>
2022-11-17 19:18:33 -0500

    static components setup

---
+++ src/components/ArtistForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+/*
+This is a shared component that will be set from Home and used inside Game
+*/
+export const ArtistForm = () => {
+  return (
+    <div>ArtistForm</div>
+  )
+}
---
+++ src/components/SongForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+/*
+This is a shared component between Home and Gameplay home will set/pass props based on user selections
+*/ 
+export const SongForm = () => {
+  return (
+    <div>SongForm</div>
+  )
+}
