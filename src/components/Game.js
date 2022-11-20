47c10144672f3996dbfe275db1149edb6956eba4
Jason Kuffler <jakuffler@gmail.com>
2022-11-19 23:24:18 -0500

    quick setup for game and score components

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react"
+// import Score from "./Score" <<-- will be rendered after final round
 
 export const Game = () => {
   return (
-    <div>Game</div>
+    <main>
+    <section>
+      <h2>Round ?¿</h2>
+
+    </section>
+    <section>
+      <Score/>
+    </section>
+    </main>
   )
 }
