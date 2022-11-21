1cd08a70fc9431e66e8757b17898f7b1bf1803b5
Jason Kuffler <jakuffler@gmail.com>
2022-11-21 14:27:19 -0500

    fetching track recommendations

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,33 @@
-import React, { useEffect, useState } from "react";
-import fetchFromSpotify, { request } from "../services/api";
-import ArtistForm from "./ArtistForm";
-import SongForm from "./SongForm";
-import { useHistory } from "react-router-dom";
+import React, { useEffect, useState } from "react"
+import fetchFromSpotify, { request } from "../services/api"
+import ArtistForm from "./ArtistForm"
+import SongForm from "./SongForm"
+import { useHistory } from "react-router-dom"
 
 const AUTH_ENDPOINT =
-  "https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token";
-const TOKEN_KEY = "whos-who-access-token";
+  "https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token"
+const TOKEN_KEY = "whos-who-access-token"
 
 const Home = () => {
-  const [genres, setGenres] = useState([]);
-  const [selectedGenre, setSelectedGenre] = useState("");
-  const [artists, setArtists] = useState([]);
-  const [selectedArtist, setSelectedArtist] = useState("");
-  const [authLoading, setAuthLoading] = useState(false);
-  const [configLoading, setConfigLoading] = useState(false);
-  const [token, setToken] = useState("");
+  const [genres, setGenres] = useState([])
+  const [selectedGenre, setSelectedGenre] = useState("")
+  const [artists, setArtists] = useState([])
+  const [selectedArtist, setSelectedArtist] = useState("")
+  const [authLoading, setAuthLoading] = useState(false)
+  const [configLoading, setConfigLoading] = useState(false)
+  const [token, setToken] = useState("")
 
-  const loadGenres = async (t) => {
-    setConfigLoading(true);
+  const loadGenres = async t => {
+    setConfigLoading(true)
     const response = await fetchFromSpotify({
       token: t,
       endpoint: "recommendations/available-genre-seeds",
-    });
-    console.log(response);
-    setGenres(response.genres);
-    setConfigLoading(false);
-  };
-  const loadArtists = async t => {
-    setConfigLoading(true);
+    })
+    console.log(response)
+    setGenres(response.genres)
+    setConfigLoading(false)
+  }
+  const loadTracks = async t => {
     const response = await fetchFromSpotify({
       token: t,
       endpoint: "recommendations",
@@ -37,53 +36,54 @@ const Home = () => {
         seed_genres: "anime",
         limit: 10,
       },
-    }).then(({ artists }) => setArtists(artists))
-    // console.log(artists);
-    // setArtists(response.artists);
-    setConfigLoading(false);
-  };
+    })
+    // .then(({ artists }) => setArtists(artists))
+    setArtists(response)
+    console.log(response)
+    // setConfigLoading(false)
+  }
 
   useEffect(() => {
-    setAuthLoading(true);
+    setAuthLoading(true)
 
-    const storedTokenString = localStorage.getItem(TOKEN_KEY);
+    const storedTokenString = localStorage.getItem(TOKEN_KEY)
     if (storedTokenString) {
-      const storedToken = JSON.parse(storedTokenString);
+      const storedToken = JSON.parse(storedTokenString)
       if (storedToken.expiration > Date.now()) {
-        console.log("Token found in localstorage");
-        setAuthLoading(false);
-        setToken(storedToken.value);
-        loadGenres(storedToken.value);
-        loadArtists(storedToken.value)
-        return;
+        console.log("Token found in localstorage")
+        setAuthLoading(false)
+        setToken(storedToken.value)
+        loadGenres(storedToken.value)
+        loadTracks(storedToken.value)
+        return
       }
     }
-    console.log("Sending request to AWS endpoint");
+    console.log("Sending request to AWS endpoint")
     request(AUTH_ENDPOINT).then(({ access_token, expires_in }) => {
       const newToken = {
         value: access_token,
         expiration: Date.now() + (expires_in - 20) * 1000,
-      };
-      localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken));
-      setAuthLoading(false);
-      setToken(newToken.value);
-      loadGenres(newToken.value);
-      loadArtists(newToken.value)
-    });
-  }, []);
+      }
+      localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken))
+      setAuthLoading(false)
+      setToken(newToken.value)
+      loadGenres(newToken.value)
+      loadTracks(newToken.value)
+    })
+  }, [])
 
   if (authLoading || configLoading) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
 
-  const history = useHistory();
+  const history = useHistory()
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    localStorage.setItem("selectedGenre", JSON.stringify(selectedGenre));
+  const handleSubmit = event => {
+    event.preventDefault()
+    localStorage.setItem("selectedGenre", JSON.stringify(selectedGenre))
     // 👇️ redirect to game screen
-    history.push("/game");
-  };
+    history.push("/game")
+  }
 
   return (
     <div>
@@ -92,10 +92,10 @@ const Home = () => {
           Genre:
           <select
             value={selectedGenre}
-            onChange={(event) => setSelectedGenre(event.target.value)}
+            onChange={event => setSelectedGenre(event.target.value)}
           >
             <option value="" />
-            {genres.map((genre) => (
+            {genres.map(genre => (
               <option key={genre} value={genre}>
                 {genre}
               </option>
@@ -105,13 +105,13 @@ const Home = () => {
         <ArtistForm />
         <SongForm />
         <br />
-        <button onClick={loadArtists}  type="submit">
+        <button onClick={loadTracks} type="submit">
           P L A Y
         </button>
       </form>
-      <button onClick={console.log('hello')}>Log</button>
+      <button onClick={() => console.log(artists)}>Log</button>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
